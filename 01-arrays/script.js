12e46f4c242b45329b2bc44512542a6e5ebb876e
AlexanderYepez-code <alexsaomn17@gmail.com>
2025-10-29 14:54:37 +0100

    eserizio completo

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -26,13 +26,28 @@ const firstTeacher = teachers[0];
 console.log(firstTeacher)
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa")
+console.log(teachers)
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-
+teachers.unshift(`Sarah`)
+console.log(teachers)
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers[6];
+console.log(lewisIndex)
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0;
+console.log(isTeachersEmpty)
+// for (let i = 0; i < teachers.length ; i++){
+  
+//   if (teachers.length=== 0){
+//     isTeachersEmpty = false
+//   }else {
+//     isTeachersEmpty = true
+//   }
+
+// }
+// console.log(isTeachersEmpty)
