26225a39b7a8cd6fa1160136e79a4402ff147674
AlexanderYepez-code <alexsaomn17@gmail.com>
2025-10-29 16:44:48 +0100

    eserizio bonus quasi teminato

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,13 +10,38 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+//Metodo tradizionale
+// const reversedTeachers = teachers.reverse();
+// console.log(reversedTeachers)
+//metodo con For
+// const teacherR = teachers.length - 1
+// for (let i = teacherR; i >=0; i--) {
+
+//   const teachersReverse = teachers[i];
+//   console.log(i, teachersReverse);
+// }
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+//Metodo con propieta di ARRAY
+const longNames = teachers.filter(nome => nome.length >= 5);
+console.log(longNames)
+// Metodo con For 
+let nomeL = [];
+for (let i = 0; i < teachers.length - 1; i++) {
+
+  const nomiLunghi = teachers[i]
+  
+  if (nomiLunghi.length >= 5) {
+    nomeL.push(nomiLunghi)
+  }
+}
+console.log(nomeL)
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
+delete teachers[1]
+console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
